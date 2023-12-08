d05c90bc5d872f947c802b4eda22a91e7d73f549
otavioobraga <otavioobraga@gmail.com>
2023-12-08 15:24:14 -0400

    Adicionado menu interativo e testes unitários

--- BankAccount/ContaBancaria.cs
+++ BankAccount/ContaBancaria.cs
@@ -1,68 +1,67 @@
-    using System;
+using System;
 
-    namespace BankAccount {
-        public class ContaBancaria {
-            public string AccNumber { get; private set; }
-            public string Name { get; private set; }
-            private double _balance;
+namespace BankAccount {
+    public class ContaBancaria {
+        public string AccNumber;
+        public string Name { get; private set; }
+        private double _balance;
 
-            /*public ContaBancaria(string accNumber, string name, double balance) {
-                AccNumber = accNumber;
-                Name = name;
-                _balance = balance;
-            }*/
+        public ContaBancaria() {
+        }
 
-            public double Balance {
-                get => _balance;
-                set {}
-            }
+        public double Balance {
+            get => _balance;
+            set { }
+        }
 
-            public string Account {
-                get => AccNumber;
-                set {
-                    Console.Write("Haverá deposito inicial ? (s/n)");
-                    char response = Console.ReadLine()[0];
-                    if (response == 's' || response == 'S') {
-                        Console.WriteLine("Digite o valor do depósito inicial: ");
-                        double balance = double.Parse(Console.ReadLine());
-                        if (balance >= 0) {
-                            _balance = balance;
-                        }
-                        else {
-                            Console.WriteLine("O valor do depósito inicial deve ser maior ou igual a 0: ");
-                        }
+        public string Account {
+            get => AccNumber;
+            set {
+                Console.Write("Haverá depósito inicial? (s/n)");
+                char response = Console.ReadLine()[0];
+                if (response == 's' || response == 'S') {
+                    Console.WriteLine("Digite o valor do depósito inicial: ");
+                    double initialDeposit;
+                    if (double.TryParse(Console.ReadLine(), out initialDeposit) && initialDeposit >= 0) {
+                        _balance = initialDeposit;
                     }
                     else {
-                        _balance = 0;
+                        Console.WriteLine("O valor do depósito inicial deve ser maior ou igual a 0.");
                     }
-                }
-            }
-
-            public string Nome {
-                get => Name;
-                set {
-                    if (value != null && value.Length > 1) {
-                        Name = value;
-                    }
-                }
-            }
-
-            public string add_balance(double value) {
-                _balance += value;
-                return $"Saldo atual: {_balance:C}";
-            }
-
-            public string withdraw_funds(double value) {
-                if (value <= _balance) {
-                    _balance -= value;
-                    return $"Saldo atual: {_balance:C}";
                 }
                 else {
-                    return "Saldo insuficiente";
+                    _balance = 0;
                 }
             }
-
-            public string get_acc_number() => AccNumber;
-            public string get_owner_name() => Name;
         }
+
+
+
+        public string Nome {
+            get => Name;
+            set {
+                if (value != null && value.Length > 1) {
+                    Name = value;
+                }
+            }
+        }
+
+        public string add_balance(double value) {
+            _balance += value;
+            return $"Saldo atual: {_balance:C}";
+        }
+
+        public string withdraw_funds(double value) {
+            if (value > 0 && value <= _balance) {
+                _balance -= value;
+                return $"Saldo atual: {_balance:C}";
+            }
+            else {
+                return "Saldo insuficiente ou valor invalido";
+            }
+        }
+
+        public string get_acc_number() => AccNumber;
+        public string get_owner_name() => Name;
     }
+}
---
+++ BankTests/Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using BankAccount;
+
+namespace BankTests {
+    [TestFixture]
+    public class ContaBancariaTests {
+        [Test]
+        public void AddBalance_ShouldIncreaseBalance() {
+            // Arrange
+            ContaBancaria conta = new ContaBancaria();
+
+            // Act
+            string result = conta.add_balance(100);
+
+            // Assert
+            Assert.AreEqual("Saldo atual: \u00a3100.00", result);
+        }
+
+        [Test]
+        public void WithdrawFunds_WithSufficientBalance_ShouldDecreaseBalance() {
+            // Arrange
+            ContaBancaria conta = new ContaBancaria();
+            conta.add_balance(200);
+
+            // Act
+            string result = conta.withdraw_funds(50);
+
+            // Assert
+            Assert.AreEqual("Saldo atual: 150,00", result);
+        }
+
+        [Test]
+        public void WithdrawFunds_WithInsufficientBalance_ShouldReturnErrorMessage() {
+            // Arrange
+            ContaBancaria conta = new ContaBancaria();
+            conta.add_balance(50);
+
+            // Act
+            string result = conta.withdraw_funds(100);
+
+            // Assert
+            Assert.AreEqual("Saldo insuficiente ou valor inválido", result);
+        }
+    }
+}
